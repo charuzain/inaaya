a6ba38b841055a70324e731d5d5e591768e0bf52
charuzain <charuabhizain@gmail.com>
2025-06-07 02:41:36 -0400

    Render product cards with image, name and price

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { fetchProducts } from '../../slice/productSlice';
 import styles from './ProductList.module.css';
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const { error, products, status } = useAppSelector((state) => state.product);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return <p>Loading....</p>;
+  }
+  if (status === 'failed') {
+    return <p>Error...</p>;
+  }
+  return (
+    <>
+      {products.map((product) => (
+        <article key={product.id} className={styles['product-card']}>
+          <div>
+            <img
+              src={`/src${product.image} `}
+              alt={product.name}
+              className={styles['product-image']}
+            />
+          </div>
+          <p>{product.name}</p>
+          <p>{`$${product.price}`}</p>
+        </article>
+      ))}
+    </>
+  );
 };
 
 export default ProductList;
