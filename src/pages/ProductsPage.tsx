4a6d40d8c25c9827aec326ec45fd21974d65dacc
charuzain <charuabhizain@gmail.com>
2025-06-07 02:40:16 -0400

    Fetches product data using fetchProducts thunk

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import SearchBar from '../components/SearchBar/SearchBar';
+import CategoryDropDown from '../components/CategoryDropDown/CategoryDropDown';
+import PriceFilter from '../components/PriceFilter/PriceFilter';
+import SortFilter from '../components/SortFilter/SortFilter';
+import ProductList from '../components/ProductList/ProductList';
+import styles from './ProductsPage.module.css';
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
-  )
-}
+    <main className={styles['product-page']}>
+      <div className={styles['filters-wrapper']}>
+        <SearchBar />
+        <CategoryDropDown />
+        <PriceFilter />
+      </div>
+      <div className={styles['result-section']}>
+        <div className={styles['result-header']}>
+          <p className={styles['result-count']}>0 product found</p>
+          <SortFilter />
+        </div>
+        <div className={styles['list-wrapper']}>
+          <ProductList />
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
