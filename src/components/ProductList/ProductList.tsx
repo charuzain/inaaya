016d39b0fcdfb3b533f2e26fbdec8542f1f91f27
charuzain <charuabhizain@gmail.com>
2025-06-08 14:49:22 -0400

    Style Quick Shop button and implement Quick View functionality using selectedProduct in Redux store

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,9 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { fetchProducts } from '../../slice/productSlice';
+import { fetchProducts, setSlectedProduct } from '../../slice/productSlice';
+import { GoHeart, GoHeartFill } from 'react-icons/go';
+
 import styles from './ProductList.module.css';
 const ProductList = () => {
-  const { error, products, status } = useAppSelector((state) => state.product);
+  const { error, products, status, selectedProduct } = useAppSelector(
+    (state) => state.product
+  );
+
+  console.log(selectedProduct);
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -20,17 +27,31 @@ const ProductList = () => {
     <>
       {products.map((product) => (
         <article key={product.id} className={styles['product-card']}>
+          <GoHeart className={styles['favourite-icon']} />
           <div className={styles['image-container']}>
             <img
               src={`/src${product.image} `}
               alt={product.name}
               className={styles['product-image']}
             />
+            <button
+              className={styles['view-btn']}
+              onClick={() =>dispatch(setSlectedProduct(product))}
+            >
+              Quick View
+            </button>
           </div>
           <p className={styles['product-title']}>{product.name}</p>
           <p className={styles['product-price']}>{`$${product.price}`}</p>
         </article>
       ))}
+      {selectedProduct && (
+        <section>
+          <div>
+            <h1>Quick Shop</h1>
+          </div>
+        </section>
+      )}
     </>
   );
 };
