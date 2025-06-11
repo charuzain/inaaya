908858ce08f04ee1a7396f7bc65a5177008eebe6
charuzain <charuabhizain@gmail.com>
2025-06-11 13:11:20 -0400

    implement updateProducts thunk to reduce stock quantities after checkout

--- src/slice/productSlice.ts
+++ src/slice/productSlice.ts
@@ -1,4 +1,8 @@
-import { createAsyncThunk, createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from '@reduxjs/toolkit';
 
 export type Size = {
   s: number;
@@ -55,6 +59,40 @@ export const fetchProducts = createAsyncThunk<
   }
 });
 
+export const updateProducts = createAsyncThunk(
+  'update/products',
+  async (_, { getState, rejectWithValue }) => {
+    const state = getState();
+    const orderedItems = state.cart.items;
+    console.log(state);
+    console.log(orderedItems);
+
+    try {
+      if (orderedItems.length > 0) {
+        for (let item of orderedItems) {
+          const res = await fetch(`http://localhost:3000/products/${item.id}`);
+          const product = await res.json();
+          console.log(product);
+          const updatedSize = {
+            ...product.sizes,
+            [item.size]: product.sizes[item.size] - item.quantity,
+          };
+          await fetch(`http://localhost:3000/products/${item.id}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ sizes: updatedSize }),
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue('Failed to update stock after checkout');
+    }
+  }
+);
+
 export const productSlice = createSlice({
   name: 'product',
   initialState,
@@ -79,8 +117,20 @@ export const productSlice = createSlice({
       .addCase(fetchProducts.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message ?? 'Something went wrong';
+      })
+      .addCase(updateProducts.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(updateProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? 'Something went wrong';
+      })
+      .addCase(updateProducts.fulfilled, (state) => {
+        state.status = 'succeeded';
       });
   },
 });
-export const { setSelectedProduct, clearSelectedProduct } = productSlice.actions;
+export const { setSelectedProduct, clearSelectedProduct } =
+  productSlice.actions;
 export default productSlice.reducer;
