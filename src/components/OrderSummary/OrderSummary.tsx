e18c85bbcea18cd906a2a899c08889841dcc79af
charuzain <charuabhizain@gmail.com>
2025-06-12 14:38:28 -0400

    Update placeOrderHandler to unwrap updateProducts thunk and navigate to confirmation on success

--- src/components/OrderSummary/OrderSummary.tsx
+++ src/components/OrderSummary/OrderSummary.tsx
@@ -1,19 +1,29 @@
+import { useNavigate } from 'react-router';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { updateProducts } from '../../slice/productSlice';
 import styles from './OrderSummary.module.css';
 
 const OrderSummary = () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
   const { items } = useAppSelector((state) => state.cart);
 
+  const placeOrderHandler = async () => {
+    try {
+      const res = await dispatch(updateProducts()).unwrap();
+      console.log(res);
+      navigate('/confirmation', { replace: true });
+    } catch (error) {
+      console.error('Order failed:', error);
+    }
+  };
+  // When we navigate with { replace: true }, the current entry in the browser's history stack is replaced with the new location, rather than pushing a new entry onto the stack. This means that when the user presses the browser's back button, they won't go back to the checkout page; instead, they will go to the page before the checkout page (product listing).
 
   const subTotal: number =
     items.length > 0 ? items.reduce((a, c) => a + c.quantity * c.price, 0) : 0;
   const calculatedTaxes: number = (13 * subTotal) / 100;
   const orderTotal: number = subTotal + calculatedTaxes;
-
-
 
   return (
     <section className={styles['summary-section']}>
@@ -34,7 +44,7 @@ const OrderSummary = () => {
         <p> Order Total</p>
         <p>CA ${orderTotal.toFixed(2)}</p>
       </div>
-      <button onClick={() => dispatch(updateProducts())}>Place Order</button>
+      <button onClick={() => placeOrderHandler()}>Place Order</button>
     </section>
   );
 };
