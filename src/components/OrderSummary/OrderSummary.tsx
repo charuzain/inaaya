6978051e90cb51fe22d28e5d360915a5daaddf61
charuzain <charuabhizain@gmail.com>
2025-06-12 00:06:19 -0400

    add logic to calculate subtotal , taxes and order total

--- src/components/OrderSummary/OrderSummary.tsx
+++ src/components/OrderSummary/OrderSummary.tsx
@@ -1,15 +1,25 @@
-import { useAppDispatch } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { updateProducts } from '../../slice/productSlice';
 import styles from './OrderSummary.module.css';
 
 const OrderSummary = () => {
   const dispatch = useAppDispatch();
+
+  const { items } = useAppSelector((state) => state.cart);
+
+  const subTotal: number =
+    items.length > 0 ? items.reduce((a, c) => a + c.quantity * c.price, 0) : 0;
+  const calculatedTaxes: number = (13 * subTotal) / 100;
+  const orderTotal: number = subTotal + calculatedTaxes;
+
+  console.log(subTotal);
+
   return (
     <section className={styles['summary-section']}>
       <h2 className={styles['summary-title']}>Order Summary</h2>
       <div className={styles['order-detail']}>
         <p>Subtotal</p>
-        <p>$123</p>
+        <p>${subTotal.toFixed(2)}</p>
       </div>
       <div className={styles['order-detail']}>
         <p>Shipping</p>
@@ -17,11 +27,11 @@ const OrderSummary = () => {
       </div>
       <div className={styles['order-detail']}>
         <p>Taxes(13%)</p>
-        <p>$124</p>
+        <p>${calculatedTaxes.toFixed(2)}</p>
       </div>
       <div className={styles['order-detail']}>
         <p> Order Total</p>
-        <p>CA $2342.32</p>
+        <p>CA ${orderTotal.toFixed(2)}</p>
       </div>
       <button onClick={() => dispatch(updateProducts())}>Place Order</button>
     </section>
