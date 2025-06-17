25306182453de87132b1f5c6883ab7dd3c4ff5e7
charuzain <charuabhizain@gmail.com>
2025-06-16 20:32:43 -0400

    add filteredProduct selector to filter, search, and sort products

--- src/selectors/filterSelector.ts
+++ src/selectors/filterSelector.ts
@@ -11,8 +11,8 @@ export const selectSearchTerm = (state: RootState) => state.filter.searchTerm;
 export const selectSort = (state: RootState) => state.filter.sort;
 
 export const filteredProduct = createSelector(
-  [selectAllProducts, selectSearchTerm, selectCategory],
-  (products, searchTerm, category): Product[] => {
+  [selectAllProducts, selectSearchTerm, selectCategory, selectSort],
+  (products, searchTerm, category, { sortBy, order }): Product[] => {
     let productList = [...products];
 
     if (searchTerm) {
@@ -20,6 +20,22 @@ export const filteredProduct = createSelector(
         item.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
+    console.log(sortBy);
+    console.log(order);
+    if (sortBy === 'price' && order === 'asc') {
+      productList.sort((a, b) => a.price - b.price);
+    }
+    if (sortBy === 'price' && order === 'desc') {
+      productList.sort((a, b) => b.price - a.price);
+    }
+    if (sortBy === 'name' && order === 'asc') {
+      productList.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (sortBy === 'name' && order === 'desc') {
+      productList.sort((a, b) => b.name.localeCompare(a.name));
+    }
+    // console.log(productList)
 
     return category === 'all'
       ? productList
