3ed14c84406b456cc14caf1a1efa6e701d7e3f7a
charuzain <charuabhizain@gmail.com>
2025-06-16 20:31:47 -0400

    add SortFilter component with sortBy and order selection using Redux

--- src/components/SortFilter/SortFilter.tsx
+++ src/components/SortFilter/SortFilter.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import type { RootState } from '../../app/store';
+import { setSortTerm } from '../../slice/filterSlice';
+import type { Order } from '../../slice/filterSlice';
 import styles from './SortFilter.module.css';
 const SortFilter = () => {
-  return <div>SortFilter</div>;
+  const dispatch = useAppDispatch();
+
+  const val = useAppSelector((state: RootState) => state.filter.sort)
+  console.log(val)
+
+  const sortChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const [sortBy, order] = e.target.value.split('-');
+    console.log(e.target.value)
+    console.log(order)
+    console.log(sortBy)
+    if (order === 'asc' || order === 'desc') {
+      dispatch(setSortTerm({ sortBy, order: order }));
+    }
+  };
+  return (
+    <div>
+      <label htmlFor="sort">SortBy</label>
+      <select
+        
+        name="sort"
+        id="sort"
+        onChange={sortChangeHandler}
+        value={`${val.sortBy}-${val.order}`}
+      >
+        <option value="price-desc">Price(High to Low)</option>
+        <option value="price-asc">Price(Low to High)</option>
+        <option value="name-asc">Name(A-Z)</option>
+        <option value="name-desc">Name(Z-A)</option>
+      </select>
+    </div>
+  );
 };
 
 export default SortFilter;
