09e7c4d35d2a011df3226befd1553f4469c3eaee
charuzain <charuabhizain@gmail.com>
2025-06-17 10:47:24 -0400

    use Redux state in PriceFilter to manage selectedPrice and maxPrice

--- src/components/PriceFilter/PriceFilter.tsx
+++ src/components/PriceFilter/PriceFilter.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
-import styles from './PriceFilter.module.css';
+// import styles from './PriceFilter.module.css';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import type { RootState } from '../../app/store';
+import { setSelectedPrice } from '../../slice/filterSlice';
+
 const PriceFilter = () => {
-  return <div>PriceFilter</div>;
+  const { maxPrice, selectedPrice } = useAppSelector(
+    (state: RootState) => state.filter
+  );
+
+  const dispatch = useAppDispatch();
+
+  if (maxPrice === 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <p>Price</p>
+      <p>$0 {selectedPrice > 0 && `- ${selectedPrice}`}</p>
+      <input
+        type="range"
+        min={0}
+        max={maxPrice}
+        value={selectedPrice}
+        onChange={(e) => dispatch(setSelectedPrice(Number(e.target.value)))}
+      />
+    </>
+  );
 };
 
 export default PriceFilter;
