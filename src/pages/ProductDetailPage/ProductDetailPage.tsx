c40e1e7dfbd19c8b3378529693219b5fa6f43093
charuzain <charuabhizain@gmail.com>
2025-06-20 14:55:31 -0400

    Display product info on product detail page

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -8,6 +8,9 @@ import type { CartItem, Sizekey } from '../../types/cartItem';
 import SizeSelector from '../../components/SizeSelector/SizeSelector';
 import QuantityButton from '../../components/QuantityButton/QuantityButton';
 import AddToCart from '../../components/AddToCart/AddToCart';
+import { PiKeyReturn } from 'react-icons/pi';
+import { HiOutlineTruck } from 'react-icons/hi2';
+import { IoStorefrontOutline } from 'react-icons/io5';
 
 const ProductDetailPage = () => {
   const { productId } = useParams();
@@ -39,7 +42,6 @@ const ProductDetailPage = () => {
 
   const availableStock = stock - alreadyInCart;
 
-
   const increaseQtyHandler = (): void => {
     setSelectedQty(selectedQty + 1);
   };
@@ -70,70 +72,117 @@ const ProductDetailPage = () => {
   }
 
   return (
-    <>
-      <div>
+    <div className={styles['container']}>
+      <section className={styles['img-container']}>
         <img
           src={`/src/${selectedProduct?.image}`}
           alt={selectedProduct?.name}
+          className={styles['image']}
         />
-      </div>
-      <div>
-        <SizeSelector
-          sizes={selectedProduct.sizes}
-          onSelect={handleSizeSelect}
-        />
-        {selectedSize && (
-          <div>
-            Selected Size : <span>{selectedSize.toUpperCase()}</span>
-          </div>
-        )}
-      </div>
-      <div>
-        <span className={styles['stock-status']}></span>
-        {selectedSize && stock !== 0 && (
-          <p className={styles['stock-count']}>
-            {stock} item(s) available in selected size .
-            {alreadyInCart > 0 && availableStock > 0 && (
-              <span>
-                {`You already have ${alreadyInCart} item(s) in cart.`}
-              </span>
-            )}
+      </section>
+      <div className={styles['detail-container']}>
+        <section className={styles['info-container']}>
+          <h1>{selectedProduct.name}</h1>
+          <p>$ {selectedProduct.price}</p>
+          <p>#{`${selectedProduct.category[0]}123-${selectedProduct.id}LJ`}</p>
+          <p>
+            <span>Fit:</span> Sizes 16-24
           </p>
-        )}
+          <div>
+            <SizeSelector
+              sizes={selectedProduct.sizes}
+              onSelect={handleSizeSelect}
+            />
+            {selectedSize && (
+              <div>
+                Selected Size : <span>{selectedSize.toUpperCase()}</span>
+              </div>
+            )}
+          </div>
+          <div>
+            <span className={styles['stock-status']}></span>
+            {selectedSize && stock !== 0 && (
+              <p className={styles['stock-count']}>
+                {stock} item(s) available in selected size .
+                {alreadyInCart > 0 && availableStock > 0 && (
+                  <span>
+                    {`You already have ${alreadyInCart} item(s) in cart.`}
+                  </span>
+                )}
+              </p>
+            )}
+          </div>
+          {selectedSize && stock === 0 && <span>Out of Stock</span>}
+          {selectedSize && stock !== 0 && availableStock <= 0 && (
+            <span>
+              You already added all available stock of selected size to cart.
+            </span>
+          )}
+
+          {selectedSize && stock > 0 && availableStock > 0 && (
+            <QuantityButton
+              increaseQtyHandler={increaseQtyHandler}
+              decreaseQtyHandler={decreaseQtyHandler}
+              selectedQty={selectedQty}
+              disableIncrease={
+                selectedQty === stock ||
+                !selectedSize ||
+                stock <= selectedQty + alreadyInCart
+              }
+              disableDecrease={!selectedSize || selectedQty === 1}
+            />
+          )}
+
+          <AddToCart
+            selectedSize={selectedSize}
+            stock={stock}
+            availableStock={availableStock}
+            selectedQty={selectedQty}
+            onAddSuccess={() => {
+              console.log('Resetting selected size and qty');
+              setSelectedSize(null);
+              setSelectedQty(1);
+            }}
+          />
+
+          <div>
+            <div>
+              <h4>Description</h4>
+              <p>{selectedProduct.description}</p>
+            </div>
+            <div>
+              <p>Material:</p>
+              <p> {selectedProduct.fabric}</p>
+            </div>
+            <div>
+              <p>Care & Use</p>
+              <p>
+                Hand wash in cold water with mild detergent. Use only
+                non-chlorine bleach when needed. Lay flat to dry and reshape
+                while still wet. Do not wring. Iron at low setting on reverse.
+                Exclusive of trim.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <div>
+              <PiKeyReturn />
+              <span>Easy Return</span>
+            </div>
+            <div>
+              <HiOutlineTruck />
+              <span>Fast Delivery</span>
+            </div>
+            <div>
+              <IoStorefrontOutline />
+              <span>In store pickup</span>
+            </div>
+          </div>
+        </section>
       </div>
-      {selectedSize && stock === 0 && <span>Out of Stock</span>}
-      {selectedSize && stock !== 0 && availableStock <= 0 && (
-        <span>
-          You already added all available stock of selected size to cart.
-        </span>
-      )}
-
-      {selectedSize && stock > 0 && availableStock > 0 && (
-        <QuantityButton
-          increaseQtyHandler={increaseQtyHandler}
-          decreaseQtyHandler={decreaseQtyHandler}
-          selectedQty={selectedQty}
-          disableIncrease={
-            selectedQty === stock ||
-            !selectedSize ||
-            stock <= selectedQty + alreadyInCart
-          }
-          disableDecrease={!selectedSize || selectedQty === 1}
-        />
-      )}
-
-      <AddToCart
-        selectedSize={selectedSize}
-        stock={stock}
-        availableStock={availableStock}
-        selectedQty={selectedQty}
-        onAddSuccess={() => {
-          console.log('Resetting selected size and qty');
-          setSelectedSize(null);
-          setSelectedQty(1);
-        }}
-      />
-    </>
+    </div>
   );
 };
 
