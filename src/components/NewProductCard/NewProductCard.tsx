f9fe0f9c8cf798da1dd13bc1e08dda7945dbea47
charuzain <charuabhizain@gmail.com>
2025-06-22 01:23:12 -0400

    add NewProductCard component with initial styling

---
+++ src/components/NewProductCard/NewProductCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { Product } from '../../slice/productSlice';
+import styles from './NewProductCard.module.css';
+import { GoHeart, GoHeartFill } from 'react-icons/go';
+
+const NewProductCard = ({ product }) => {
+  return (
+    <div className={styles['product-card']}>
+      <div className={styles['product-card-image-wrapper']}>
+        <img
+          src={`/src/${product.image}`}
+          alt={product.name}
+          className={styles['product-card-image']}
+        />
+
+        <GoHeart
+          className={styles['favorite-button']}
+          fill={product.favorite ? 'currentColor' : 'none'}
+          size={20}
+          strokeWidth={1.5}
+        />
+      </div>
+      <div className={styles['product-info']}>
+        <h3 className={styles['product-name']}>{product.name}</h3>
+        <p className={styles['product-price']}>{product.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NewProductCard;
