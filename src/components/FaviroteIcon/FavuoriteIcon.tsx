1a94215677b55c7a6e4030903b5f16a6f0fa5848
charuzain <charuabhizain@gmail.com>
2025-06-25 13:35:09 -0400

    show filled or empty heart icon in FavouriteIcon based on favorite product count

--- src/components/FaviroteIcon/FavuoriteIcon.tsx
+++ src/components/FaviroteIcon/FavuoriteIcon.tsx
@@ -1,10 +1,23 @@
 import { GoHeart, GoHeartFill } from 'react-icons/go';
 import styles from './Favuorite.module.css';
-
+import { useNavigate } from 'react-router';
+import { useAppSelector } from '../../app/hooks';
+import type { RootState } from '../../app/store';
 
 const FavuoriteIcon = () => {
-  const numLikes = 0;
-  return <div>{numLikes === 0 ? <GoHeart className={styles['nav-icon']} /> : <GoHeartFill />}</div>;
+  const navigate = useNavigate();
+  const numOfFavProduct = useAppSelector(
+    (state: RootState) => state.favorite.favoriteProducts.length
+  );
+  return (
+    <div onClick={() => navigate('favorite')}>
+      {numOfFavProduct === 0 ? (
+        <GoHeart className={styles['nav-icon']} />
+      ) : (
+        <GoHeartFill className={styles['nav-icon']} />
+      )}
+    </div>
+  );
 };
 
 export default FavuoriteIcon;
