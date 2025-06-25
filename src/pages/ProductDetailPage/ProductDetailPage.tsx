e1b9d6df195edbd750bc09699ab73f898ae69c23
charuzain <charuabhizain@gmail.com>
2025-06-25 13:44:41 -0400

    enable AddToCart modal on product detail page with size, quantity selection and modal close handler to reset state

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { fetchProductById } from '../../slice/productSlice';
 import type { RootState } from '../../app/store';
@@ -71,118 +71,131 @@ const ProductDetailPage = () => {
     return <h1>No detail found..</h1>;
   }
 
+  const handleModalClose = () => {
+    setSelectedSize(null);
+    setSelectedQty(1);
+  };
+
   return (
-    <div className={styles['container']}>
-      <section className={styles['img-container']}>
-        <img
-          src={`/src/${selectedProduct?.image}`}
-          alt={selectedProduct?.name}
-          className={styles['image']}
-        />
+    <>
+      <section className={styles['breadcrumb-container']}>
+        <div className={styles['breadcrumb']}>
+          <Link to="/">Home</Link>{selectedProduct && <Link to="/products">/Products</Link>}/
+          {selectedProduct.name}
+        </div>
       </section>
-      <div className={styles['detail-container']}>
-        <section className={styles['info-container']}>
-          <h1>{selectedProduct.name}</h1>
-          <p>$ {selectedProduct.price}</p>
-          <p>#{`${selectedProduct.category[0]}123-${selectedProduct.id}LJ`}</p>
-          <p>
-            <span>Fit:</span> Sizes 16-24
-          </p>
-          <div>
-            <SizeSelector
-              sizes={selectedProduct.sizes}
-              onSelect={handleSizeSelect}
-            />
-            {selectedSize && (
-              <div>
-                Selected Size : <span>{selectedSize.toUpperCase()}</span>
-              </div>
-            )}
-          </div>
-          <div>
-            <span className={styles['stock-status']}></span>
-            {selectedSize && stock !== 0 && (
-              <p className={styles['stock-count']}>
-                {stock} item(s) available in selected size .
-                {alreadyInCart > 0 && availableStock > 0 && (
-                  <span>
-                    {`You already have ${alreadyInCart} item(s) in cart.`}
-                  </span>
-                )}
-              </p>
-            )}
-          </div>
-          {selectedSize && stock === 0 && <span>Out of Stock</span>}
-          {selectedSize && stock !== 0 && availableStock <= 0 && (
-            <span>
-              You already added all available stock of selected size to cart.
-            </span>
-          )}
 
-          {selectedSize && stock > 0 && availableStock > 0 && (
-            <QuantityButton
-              increaseQtyHandler={increaseQtyHandler}
-              decreaseQtyHandler={decreaseQtyHandler}
-              selectedQty={selectedQty}
-              disableIncrease={
-                selectedQty === stock ||
-                !selectedSize ||
-                stock <= selectedQty + alreadyInCart
-              }
-              disableDecrease={!selectedSize || selectedQty === 1}
-            />
-          )}
-
-          <AddToCart
-            selectedSize={selectedSize}
-            stock={stock}
-            availableStock={availableStock}
-            selectedQty={selectedQty}
-            onAddSuccess={() => {
-              console.log('Resetting selected size and qty');
-              setSelectedSize(null);
-              setSelectedQty(1);
-            }}
+      <div className={styles['container']}>
+        <section className={styles['img-container']}>
+          <img
+            src={`/src/${selectedProduct?.image}`}
+            alt={selectedProduct?.name}
+            className={styles['image']}
           />
+        </section>
+        <div className={styles['detail-container']}>
+          <section className={styles['info-container']}>
+            <h1>{selectedProduct.name}</h1>
+            <p>$ {selectedProduct.price}</p>
+            <p>
+              #{`${selectedProduct.category[0]}123-${selectedProduct.id}LJ`}
+            </p>
+            <p>
+              <span>Fit:</span> Sizes 16-24
+            </p>
+            <div>
+              <SizeSelector
+                sizes={selectedProduct.sizes}
+                onSelect={handleSizeSelect}
+              />
+              {selectedSize && (
+                <div>
+                  Selected Size : <span>{selectedSize.toUpperCase()}</span>
+                </div>
+              )}
+            </div>
+            <div>
+              <span className={styles['stock-status']}></span>
+              {selectedSize && stock !== 0 && (
+                <p className={styles['stock-count']}>
+                  {stock} item(s) available in selected size .
+                  {alreadyInCart > 0 && availableStock > 0 && (
+                    <span>
+                      {`You already have ${alreadyInCart} item(s) in cart.`}
+                    </span>
+                  )}
+                </p>
+              )}
+            </div>
+            {selectedSize && stock === 0 && <span>Out of Stock</span>}
+            {selectedSize && stock !== 0 && availableStock <= 0 && (
+              <span>
+                You already added all available stock of selected size to cart.
+              </span>
+            )}
 
-          <div>
+            {selectedSize && stock > 0 && availableStock > 0 && (
+              <QuantityButton
+                increaseQtyHandler={increaseQtyHandler}
+                decreaseQtyHandler={decreaseQtyHandler}
+                selectedQty={selectedQty}
+                disableIncrease={
+                  selectedQty === stock ||
+                  !selectedSize ||
+                  stock <= selectedQty + alreadyInCart
+                }
+                disableDecrease={!selectedSize || selectedQty === 1}
+              />
+            )}
+
+            <AddToCart
+              selectedSize={selectedSize}
+              stock={stock}
+              availableStock={availableStock}
+              selectedQty={selectedQty}
+              // onAddSuccess={() => {}}
+              onModalClose={handleModalClose}
+            />
+
             <div>
-              <h4>Description</h4>
-              <p>{selectedProduct.description}</p>
+              <div>
+                <h4>Description</h4>
+                <p>{selectedProduct.description}</p>
+              </div>
+              <div>
+                <p>Material:</p>
+                <p> {selectedProduct.fabric}</p>
+              </div>
+              <div>
+                <p>Care & Use</p>
+                <p>
+                  Hand wash in cold water with mild detergent. Use only
+                  non-chlorine bleach when needed. Lay flat to dry and reshape
+                  while still wet. Do not wring. Iron at low setting on reverse.
+                  Exclusive of trim.
+                </p>
+              </div>
             </div>
+          </section>
+          <section>
             <div>
-              <p>Material:</p>
-              <p> {selectedProduct.fabric}</p>
+              <div>
+                <PiKeyReturn />
+                <span>Easy Return</span>
+              </div>
+              <div>
+                <HiOutlineTruck />
+                <span>Fast Delivery</span>
+              </div>
+              <div>
+                <IoStorefrontOutline />
+                <span>In store pickup</span>
+              </div>
             </div>
-            <div>
-              <p>Care & Use</p>
-              <p>
-                Hand wash in cold water with mild detergent. Use only
-                non-chlorine bleach when needed. Lay flat to dry and reshape
-                while still wet. Do not wring. Iron at low setting on reverse.
-                Exclusive of trim.
-              </p>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div>
-            <div>
-              <PiKeyReturn />
-              <span>Easy Return</span>
-            </div>
-            <div>
-              <HiOutlineTruck />
-              <span>Fast Delivery</span>
-            </div>
-            <div>
-              <IoStorefrontOutline />
-              <span>In store pickup</span>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
