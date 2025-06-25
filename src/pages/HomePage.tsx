16451698c90614b6c28a80b2f1dc4ba3ee08b403
charuzain <charuabhizain@gmail.com>
2025-06-25 13:41:47 -0400

    create HomePage layout with hero, categories, slogan marquee, and product slider

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,14 @@
 import styles from './HomePage.module.css';
-import heroImage from './../assets/images/about3.png';
-import { MdArrowRightAlt } from 'react-icons/md';
 import Category from '../components/Category/Category';
 import ProductSlider from '../components/ProductSlider/ProductSlider';
 import SloganMarquee from '../components/SloganMarquee/SloganMarquee';
 
+import HeroSection from '../components/Hero/HeroSection';
+
 const HomePage = () => {
   return (
     <main className={styles['main']}>
-      <div className={styles['hero-section']}>
-        <section className={styles['hero-text']}>
-          <h1 className={styles['hero-title']}>
-            Fashion That Fits Your Curves, Flatter Your Style.
-          </h1>
-          <p className={styles['hero-subtitle']}>
-            Discover a world where fashion meets comfort, designed exclusively
-            to celebrate every curve. Embrace styles that empower you to look
-            and feel your best, every single day
-          </p>
-          <button className={styles['hero-cta-button']}>
-            Shop Now <MdArrowRightAlt />
-          </button>
-        </section>
-        <section className={styles['hero-image-container']}>
-          <img
-            src={heroImage}
-            alt="Hero Image"
-            className={styles['hero-image']}
-          />
-        </section>
-      </div>
+      <HeroSection />
 
       <section className={styles['category-section']}>
         <Category />
