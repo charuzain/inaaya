e3d575c629a1cb6e37e7e2d034f13484ea173d96
charuzain <charuabhizain@gmail.com>
2025-06-25 16:52:17 -0400

    implement addToSaveForLater reducer to add unique products to saved list

--- src/slice/savedForLaterSlice.ts
+++ src/slice/savedForLaterSlice.ts
@@ -1,12 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
-const initialState = {
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { CartItemType } from '../types/cartItem';
+type StateForLaterState = {
+  savedProducts: CartItemType[];
+};
+const initialState: StateForLaterState = {
   savedProducts: [],
 };
+
 export const savedForLaterSlice = createSlice({
   name: 'savedProduct',
   initialState,
   reducers: {
-    addToSaveForLater: (state, action) => {},
+    addToSaveForLater: (state, action: PayloadAction<CartItemType>) => {
+      const itemExist = state.savedProducts.find(
+        (product) =>
+          product.id === action.payload.id &&
+          product.size === action.payload.size
+      );
+      if (!itemExist) {
+        state.savedProducts.push(action.payload);
+      }
+    },
     removeFromSaveToLater: (state, action) => {},
     moveToBag: (state, action) => {},
   },
